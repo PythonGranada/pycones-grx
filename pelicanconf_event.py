3fdc721c017e1077b1db2a49857b3b064c6bf5c5
Jose Miguel <josemiguellopez@glampinghub.com>
2022-01-16 22:31:05 +0100

    PDF

--- pelicanconf_event.py
+++ pelicanconf_event.py
@@ -22,7 +22,7 @@ TELEGRAM_LINK = "https://t.me/pycones2022" # TODO
 
 TICKETS_LINK="#"
 CALL_FOR_PAPERS_LINK = "#"
-SPONSORS_DOSSIER = "#"
+SPONSORS_DOSSIER = "/theme/files/sponsorship_ES.pdf"
 
 # https://google-map-generator.com/
 MAP_IFRAME_LINK = "https://maps.google.com/maps?q=Granada%20Paraningo%20Salud&t=&z=17&ie=UTF8&iwloc=&output=embed"
